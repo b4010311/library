495160fc372fffc8a6650991f913e3add27d0f64
todza66 <t.d.maswera@gmail.com>
2016-03-22 13:38:33 +0000

    created domain class Library

---
+++ library/grails-app/domain/com/library/Library.groovy
@@ -0,0 +1,23 @@
+package com.library
+
+class Library {
+
+Sting location
+
+Int openingHours
+
+String book
+
+String student
+
+String librarian 
+
+String toString() {
+
+"$location, $openingHours"
+
+}
+
+    static constraints = {
+    }
+}
