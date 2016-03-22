962ac48c0c284362e14d89e1d61e08c45a318062
todza66 <t.d.maswera@gmail.com>
2016-03-22 13:15:01 +0000

    created domain class Student

---
+++ library/grails-app/domain/com/library/Student.groovy
@@ -0,0 +1,21 @@
+package com.library
+
+class Student {
+
+String name
+
+String email
+
+String studentid
+
+String course
+
+String toString(){
+
+"$name, $studentid, $email"
+
+}
+
+    static constraints = {
+    }
+}
