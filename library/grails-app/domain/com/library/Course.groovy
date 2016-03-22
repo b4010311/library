1ab20a60e58c3529e325f2049d49eea4821adeeb
todza66 <t.d.maswera@gmail.com>
2016-03-22 13:03:50 +0000

    created domain class Course

---
+++ library/grails-app/domain/com/library/Course.groovy
@@ -0,0 +1,23 @@
+package com.library
+
+class Course {
+
+String title
+
+String code
+
+String tutor
+
+String department
+
+String description
+
+String toString(){
+
+"$title, $code, $department"
+
+}
+
+    static constraints = {
+    }
+}
