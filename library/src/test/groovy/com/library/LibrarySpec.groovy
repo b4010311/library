211420e79d6814716c2e8ca40af20315d545e789
todza66 <t.d.maswera@gmail.com>
2016-03-22 17:57:52 +0000

    Test Successful for domain class Library

--- library/src/test/groovy/com/library/LibrarySpec.groovy
+++ library/src/test/groovy/com/library/LibrarySpec.groovy
@@ -15,5 +15,17 @@ class LibrarySpec extends Specification {
     def cleanup() {
     }
 
-    
+         void toString() {
+
+when: "A Library has location and openingHours"
+
+def library = new Library(location:'Sheffield',
+
+                 openingHours:'9-5pm Monday to Friday and 10-4pm Saturday and Sunday')
+
+then: "the to String method will merge them."
+
+library.toString() == 'Sheffield, 9-5pm Monday to Friday and 10-4pm Saturday and Sunday'
+        
+    }
 }
