a4615dd94fd1d431f55897e623fc822389510d05
todza66 <t.d.maswera@gmail.com>
2016-03-22 13:24:54 +0000

    created domain class Book

---
+++ library/grails-app/domain/com/library/Book.groovy
@@ -0,0 +1,25 @@
+package com.library
+
+class Book {
+
+String title
+
+String author
+
+Int isbn
+
+String dateBorrowed
+
+String returnDate
+
+String student
+
+String toString(){
+
+"$title, $author"
+
+}
+
+    static constraints = {
+    }
+}
