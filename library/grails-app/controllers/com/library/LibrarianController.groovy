d2e750d82b3ce5e5689330b00a4e08093d52ac82
todza66 <t.d.maswera@gmail.com>
2016-03-24 18:43:04 +0000

    Created Login and Logout and validated it for Librarian

--- library/grails-app/controllers/com/library/LibrarianController.groovy
+++ library/grails-app/controllers/com/library/LibrarianController.groovy
@@ -95,6 +95,38 @@ class LibrarianController {
         }
     }
 
+def login() {
+
+		if(params.cName)
+	return [cName:params.cName, aName:params.aName]
+}
+
+def validate() {
+                  def user = Librarian.findByUserName(params.username)
+                  if (user && user.password == params.password){
+
+                  session.user = user
+                  if (params.cName) 
+
+                  redirect controller:params.cName, action:params.aName
+           
+                  else
+
+                  redirect controller:'librarian',action:'index' }
+                  
+                  else {
+                    
+                  flash.message = "invalid username and password."
+ 
+                  render view: 'login'
+                  } 
+               }
+
+def logout = {
+               session.user = null
+               redirect(uri:'/')
+             }
+
     protected void notFound() {
         request.withFormat {
             form multipartForm {
